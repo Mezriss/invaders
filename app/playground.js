71b801150fd6e24e15bb7b0fde32aaf9e1b2de5c
Mezriss <mezriss@gmail.com>
2017-05-09 15:12:43 +0300

    Mask icon, moved touch icons

--- app/playground.js
+++ app/playground.js
@@ -25,65 +25,35 @@ for (let i = 0; i < 100; i += 1) {
 	shipyard[i].show(playgroundCtx, i % 10 * 50 + 20, Math.floor(i / 10) * 50 + 20);
 	playgroundCtx.fillText(i, i % 10 * 50 + 5, Math.floor(i / 10) * 50 + 10);
 }
-function generate() {
-	[192, 512, 57, 60, 72, 76, 114, 120, 144, 152, 180, 16, 32, 150].forEach(size => {
-		const canvas = document.createElement('canvas');
-		canvas.width = size;
-		canvas.height = size;
-		const canvasCtx = canvas.getContext('2d');
-		canvasCtx.fillStyle = n2c(spaceCfg.background);
-		canvasCtx.fillRect(0, 0, size, size);
-		const i = parseInt(document.getElementById('shipNumber').value, 10);
-		const pixelSize = Math.floor(size * 0.8 / shipCfg.width);
-		ship
-			.create({
-				pixelSize,
-				blueprint: [
-					0,
-					1,
-					0,
-					0,
-					0,
-					1,
-					0,
-					1,
-					1,
-					0,
-					1,
-					0,
-					1,
-					1,
-					1,
-					1,
-					1,
-					0,
-					1,
-					1,
-					1,
-					1,
-					1,
-					0,
-					1,
-					0,
-					1,
-					1,
-					1,
-					0,
-					0,
-					1,
-					0,
-					0,
-					1
-				]
-			})
-			.show(
-				canvasCtx,
-				Math.floor((size - shipCfg.width * pixelSize) / 2),
-				Math.floor((size - shipCfg.height * pixelSize) / 2)
-			);
 
-		document.getElementById('icons').appendChild(canvas);
-	});
+function drawIcon(size, pixelSize) {
+	const canvas = document.createElement('canvas');
+	canvas.width = size;
+	canvas.height = size;
+	const canvasCtx = canvas.getContext('2d');
+	canvasCtx.fillStyle = n2c(spaceCfg.background);
+	canvasCtx.fillRect(0, 0, size, size);
+	const i = parseInt(document.getElementById('shipNumber').value, 10);
+
+	ship
+		.create({
+			pixelSize,
+			blueprint: shipyard[i].blueprint
+		})
+		.show(
+			canvasCtx,
+			Math.floor((size - shipCfg.width * pixelSize) / 2),
+			Math.floor((size - shipCfg.height * pixelSize) / 2)
+		);
+
+	document.getElementById('icons').appendChild(canvas);
+}
+
+function generate() {
+	[192, 512, 57, 60, 72, 76, 114, 120, 144, 152, 180, 16, 32, 150].forEach(size =>
+		drawIcon(size, Math.floor(size * 0.8 / shipCfg.width))
+	);
+	drawIcon(16, 2);
 }
 
 document.getElementById('generate').addEventListener('click', generate);
